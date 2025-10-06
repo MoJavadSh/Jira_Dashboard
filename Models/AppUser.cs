ff6ceb9c329b3919621e4cf2271a74febf40fc61
MohammadJavad <mojavadssh@gmail.com>
2025-10-06 15:09:08 +0330

    .

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -5,6 +5,4 @@ public class AppUser
     public long Id { get; set; }
     public string UserKey { get; set; } 
     public string LowerUserName { get; set; } 
-    public long AccountId { get; set; } 
-    public long ApplicationId { get; set; } 
 }
